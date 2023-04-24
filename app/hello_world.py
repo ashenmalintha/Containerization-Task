ac12854d1fcaaa89b27c823ead2b6e48449c238d
Your Name <you@example.com>
2023-04-24 15:40:07 +0530

    Adding README File

--- app/hello_world.py
+++ app/hello_world.py
@@ -1,21 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-    def __repr__(self):
-        return f"<User {self.username}>"
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5005)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
